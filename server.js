5f89dff3fbddefa25d75bf33d506f5b281d08d27
Lana Kim <lanakim@Lanas-MacBook-Pro.local>
2022-05-21 21:49:46 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+// ===== Routes =====
+
+// Index
+// New
+// Delete
+// Update
+// Create
+// Edit
+// Show
+
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`))
